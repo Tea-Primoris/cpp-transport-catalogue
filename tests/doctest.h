474c176189e5ba6aac85918d19e945978d56c2c2
Tea-Primoris <32336957+Tea-Primoris@users.noreply.github.com>
2023-12-23 17:04:48 +0300

    Removing TODO marks from doctest.h, so they won't annoy me anymore when committing with CLion

--- tests/doctest.h
+++ tests/doctest.h
@@ -574,7 +574,6 @@ DOCTEST_INTERFACE extern bool is_running_in_test;
 //              and the "is small" bit remains "0" ("as well as the capacity left") so its OK
 // Idea taken from this lecture about the string implementation of facebook/folly - fbstring
 // https://www.youtube.com/watch?v=kPR8h4-qZdk
-// TODO:
 // - optimizations - like not deleting memory unnecessarily in operator= and etc.
 // - resize/reserve/clear
 // - replace
@@ -1397,7 +1396,7 @@ DOCTEST_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wunused-comparison")
         bool   m_passed;
         String m_decomp;
 
-        Result() = default; // TODO: Why do we need this? (To remove NOLINT)
+        Result() = default; // Why do we need this? (To remove NOLINT)
         Result(bool passed, const String& decomposition = String());
 
         // forbidding some expressions based on this table: https://en.cppreference.com/w/cpp/language/operator_precedence
@@ -3588,7 +3587,7 @@ using ticks_t = timer_large_integer::type;
     ContextState* g_cs = nullptr;
 
     // used to avoid locks for the debug output
-    // TODO: figure out if this is indeed necessary/correct - seems like either there still
+    // figure out if this is indeed necessary/correct - seems like either there still
     // could be a race or that there wouldn't be a race even if using the context directly
     DOCTEST_THREAD_LOCAL bool g_no_colors;
 
@@ -4865,7 +4864,7 @@ namespace {
 #ifdef DOCTEST_PLATFORM_WINDOWS
 #define DOCTEST_OUTPUT_DEBUG_STRING(text) ::OutputDebugStringA(text)
 #else
-    // TODO: integration with XCode and other IDEs
+    // integration with XCode and other IDEs
 #define DOCTEST_OUTPUT_DEBUG_STRING(text)
 #endif // Platform
 
@@ -5030,7 +5029,6 @@ namespace {
 
 // =================================================================================================
 // The following code has been taken verbatim from Catch2/include/internal/catch_xmlwriter.h/cpp
-// This is done so cherry-picking bug fixes is trivial - even the style/formatting is untouched.
 // =================================================================================================
 
     class XmlEncode {
@@ -5126,7 +5124,6 @@ namespace {
 
 // =================================================================================================
 // The following code has been taken verbatim from Catch2/include/internal/catch_xmlwriter.h/cpp
-// This is done so cherry-picking bug fixes is trivial - even the style/formatting is untouched.
 // =================================================================================================
 
 using uchar = unsigned char;
@@ -5522,7 +5519,7 @@ namespace {
             if(opt.no_version == false)
                 xml.writeAttribute("version", DOCTEST_VERSION_STR);
 
-            // only the consequential ones (TODO: filters)
+            // only the consequential ones
             xml.scopedElement("Options")
                     .writeAttribute("order_by", opt.order_by.c_str())
                     .writeAttribute("rand_seed", opt.rand_seed)
@@ -5701,11 +5698,6 @@ namespace {
         }
     }
 
-    // TODO:
-    // - log_message()
-    // - respond to queries
-    // - honor remaining options
-    // - more attributes in tags
     struct JUnitReporter : public IReporter
     {
         XmlWriter xml;
@@ -6560,7 +6552,6 @@ namespace {
 
         if(type) {
             // integer
-            // TODO: change this to use std::stoi or something else! currently it uses undefined behavior - assumes '0' on failed parse...
             int theInt = std::atoi(parsedValue.c_str());
             if (theInt != 0) {
                 res = theInt; //!OCLINT parameter reassignment
@@ -6852,8 +6843,6 @@ int Context::run() {
         if(matchesAny(curr.first.second.c_str(), p->filters[8], false, p->case_sensitive))
             p->reporters_currently_used.push_back(curr.second(*g_cs));
     }
-
-    // TODO: check if there is nothing in reporters_currently_used
 
     // prepend all listeners
     for(auto& curr : getListeners())
